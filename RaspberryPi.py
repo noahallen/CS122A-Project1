15e2dae8a8b2c88811572d9f1f7c1c2dd4e98867
Noah <noahcallen@gmail.com>
2022-04-23 20:27:21 -0700

    Added a separate function

--- RaspberryPi.py
+++ RaspberryPi.py
@@ -4,23 +4,34 @@
 # dmesg | grep "tty" to find port name
 import serial
 import time
+
+
+def parseOutput(output):
+    print(output)
+
+
+
 if __name__ == '__main__':
 
     print('Running. Press CTRL-C to exit.')
-    with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
-        time.sleep(0.1)  # wait for serial to open
-        if arduino.isOpen():
-            print("{} connected!".format(arduino.port))
-            try:
-                while True:
-                    # cmd = input("Enter command : ")
-                    # arduino.write(cmd.encode())
-                    # time.sleep(0.1) #wait for arduino to answer
-                    while arduino.inWaiting() == 0:
-                        pass
-                    if arduino.inWaiting() > 0:
-                        answer = arduino.readline()
-                        print(answer.decode("Ascii"))
-                        arduino.flushInput()  # remove data after reading
-            except KeyboardInterrupt:
-                print("KeyboardInterrupt has been caught.")
+        with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
+            time.sleep(0.1)  # wait for serial to open
+            if arduino.isOpen():
+                print("{} connected!".format(arduino.port))
+                try:
+                    while True:
+                        output = ""
+                        time.sleep(0.1) #wait for arduino to answer
+                        while arduino.inWaiting() == 0:
+                            pass
+                        if arduino.inWaiting() > 0:
+                            #read output from arduino
+                            answer = arduino.readline()
+
+                            #Print output 
+                            output = answer.decode("Ascii")
+                            print(output)
+                            parseOutput(output)
+                            arduino.flushInput()  # remove data after reading
+                except KeyboardInterrupt:
+                    print("KeyboardInterrupt has been caught.")
