9ad160bceb4d4dad962ffddbc4b150a29aadf923
Noah <noahcallen@gmail.com>
2022-04-23 21:11:01 -0700

    Added basic spotify API useage

--- RaspberryPi.py
+++ RaspberryPi.py
@@ -6,6 +6,17 @@ import spotipy
 import json
 import webbrowser
 
+global clientID
+global clientSecret
+global spotifyUserName
+global redirectURL
+
+clientID = "b6dc4115b3e54349b7d02bbf3f865f85"
+clientSecret = "31d4d345bc114da39414613c1ce45a38"
+spotifyUserName = "noah_allen24"
+redirectURL = "http://google.com/"
+
+
 
 def parseOutput(output):
     print(output)
@@ -13,7 +24,14 @@ def parseOutput(output):
 
 
 if __name__ == '__main__':
-
+    oauth_object = spotipy.SpotifyOAuth(clientID,clientSecret,redirectURI)
+    token_dict = oauth_object.get_access_token()
+    token = token_dict['access_token']
+    spotifyObject = spotipy.Spotify(auth=token)
+    user = spotifyObject.current_user()
+    print(json.dumps(user,sort_keys=True, indent=4))
+    print("Welcome, "+ user['display_name'])
+    
     print('Running. Press CTRL-C to exit.')
     with serial.Serial("/dev/ttyACM0", 9600, timeout=.5) as arduino:
         time.sleep(0.1)  # wait for serial to open
