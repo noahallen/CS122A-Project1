a8cc714ada04d8d534096ea2bf67ed4f93fd05d7
Noah <noahcallen@gmail.com>
2022-04-23 20:28:30 -0700

    Fixed typo

--- RaspberryPi.py
+++ RaspberryPi.py
@@ -14,24 +14,24 @@ def parseOutput(output):
 if __name__ == '__main__':
 
     print('Running. Press CTRL-C to exit.')
-        with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
-            time.sleep(0.1)  # wait for serial to open
-            if arduino.isOpen():
-                print("{} connected!".format(arduino.port))
-                try:
-                    while True:
-                        output = ""
-                        time.sleep(0.1) #wait for arduino to answer
-                        while arduino.inWaiting() == 0:
-                            pass
-                        if arduino.inWaiting() > 0:
-                            #read output from arduino
-                            answer = arduino.readline()
+    with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
+        time.sleep(0.1)  # wait for serial to open
+        if arduino.isOpen():
+            print("{} connected!".format(arduino.port))
+            try:
+                while True:
+                    output = ""
+                    time.sleep(0.1) #wait for arduino to answer
+                    while arduino.inWaiting() == 0:
+                        pass
+                    if arduino.inWaiting() > 0:
+                        #read output from arduino
+                        answer = arduino.readline()
 
-                            #Print output 
-                            output = answer.decode("Ascii")
-                            print(output)
-                            parseOutput(output)
-                            arduino.flushInput()  # remove data after reading
-                except KeyboardInterrupt:
-                    print("KeyboardInterrupt has been caught.")
+                        #Print output 
+                        output = answer.decode("Ascii")
+                        print(output)
+                        parseOutput(output)
+                        arduino.flushInput()  # remove data after reading
+            except KeyboardInterrupt:
+                print("KeyboardInterrupt has been caught.")
